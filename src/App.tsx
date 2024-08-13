3d96372dd2473d105b4a7976dd3fdd166b80d8a2
Akhil Anthireddy <akhilnr@amazon.com>
2024-08-13 12:55:29 -0700

    added css

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from 'react';
 import type { Schema } from '../amplify/data/resource';
 import { generateClient } from 'aws-amplify/data';
-import { defineAuth } from '@aws-amplify/backend';
 import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
 const client = generateClient<Schema>();
 
